59f390c9f5d24dee0777a3d42cef0416cac89487
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2022-10-16 10:14:16 +0530

    Add files via upload

---
+++ c p1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf(" enter number a");
+scanf("%d",&a);
+   printf("enter number b");
+   scanf("%d",&b);
+   printf("the sum is %d\n ",a+b);
+   
+   
+    return 0;
+}
+
